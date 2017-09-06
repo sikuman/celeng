8f7f7db60e6ba3ff75337f12fc1c3811f77de159
sikuman <31029528+sikuman@users.noreply.github.com>
2017-09-06 17:05:06 +0700

    dasdas

--- split.php
+++ split.php
@@ -2,16 +2,13 @@
 	ini_set('memory_limit', '-1');
 // header( "Content-type: text/xml" ); 
 	$f = "xml"; // $_POST[format]
-	$per_page = 10; // max size
 	$files_name= file('keywords/keywords.txt' );
 	$priority = "0.5" ;
 	$lastmodified =  date( 'c' );
 	$changefreq ="daily";
 	$path_kb = "http://fullhdfree.xyz/";
 	$slug = "video/";
-	// $exten =".html";
-	
-	// $filectime = filectime($files_name);
+	$per_page = 10; // max size
 
 	$fn = "1"; // file number, starts empty, then 1, 2, etc
 	
@@ -21,46 +18,38 @@
 		$sitemap_header = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n".
 			"<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\" xmlns:xhtml=\"http://www.w3.org/1999/xhtml\" xmlns:image=\"http://www.google.com/schemas/sitemap-image/1.1\" xmlns:video=\"http://www.google.com/schemas/sitemap-video/1.1\">\n";
 		$sitemap_footer = "</urlset>\n";
-$sitemap = array();
-foreach($files_name as $url){
-		$sitemap_urls_string = "";
-		// if (strlen($sitemap_urls_string) + strlen($sitemap_url_string) + $baselen > $ms)
-		$hitung = count($url);
-		echo $hitung;
-		// if ($hitung > $per_page)
-		// {
-		$sitemap_url_string = "$prefix_string$path_kb$slug$url"."$postfix_string";
+
+// echo $files_name;
+
+$arraystrreplace= array("&"," ");
+$singletxt = array($files_name);
+$strreplace = str_replace($arraystrreplace,"-",$files_name); 
+
+foreach ($strreplace as $kiwod){
+	// echo $kiwod;
+			$sitemap_url_string = "$prefix_string$path_kb$slug$kiwod"."$postfix_string";
+	if (count($singletxt) == $per_page){
+		echo "jancuuk";
+	}else {
 			writeFile($sitemap_header.$sitemap_url_string.$sitemap_footer, $f, $fn);
-			// $sitemap_urls_string = "";
-		// }
-		$sitemap_urls_string .= $sitemap_url_string;
-		
-		**** array di pecah dadi 10 terus di pecah meneh lagi di write **** besok coba ini
-    // }
-}
-
-	$page_numbers = range(1,count($pages));
- foreach ($pages as $pg_num){
-	    echo "\t<sitemap>\n";
-        echo "\t\t<loc>" .htmlentities($url['loc']) . "</loc>\n";
-        echo "\t\t<lastmod>$lastmodified</lastmod>\n";
-        echo "\t</sitemap>\n";
-// echo $pg_num ."<br>";
- }
-
-// function to avoid repetition of code
-
+		// echo "wedus";
+	}		
+		}
 function writeFile($sitemap_string, $f, &$fn) // the new $fn is returned to the main code
 {
 	$result_status = "";
-
+	$files_name= file_get_contents('keywords/keywords.txt' );
+	$singletxt = array($files_name);
+echo "panggil fungsi write";
 	$fp = @fopen("./sitemap/sitemap$fn.$f", "wb"); // Create Sitemap File
 
 	if($fp) // The sitemap file opened now, let's write to it.
 	{
+	// if($singletxt == $per_page){	
 		@fwrite($fp, $sitemap_string);
 		@fclose($fp);
 		$result_status = "success";
+		// }
 	}
 	else // Failed to Open File for Writing
 	{
