27763161168b1feffc62e1ec47739633a6e9fb3c
sikuman <31029528+sikuman@users.noreply.github.com>
2017-09-13 16:08:28 +0700

    add and commit file tambahan
    dadsa

--- index.php
+++ index.php
@@ -1,40 +1,64 @@
-<?php 
+<?php
+	ini_set('memory_limit', '-1');
+// header( "Content-type: text/xml" ); 
+	$f = ".xml"; // $_POST[format]
+	$files_name= file('keywords/keywords.txt' );
+	$priority = "0.5" ;
+	$lastmodified =  date( 'c' );
+	$changefreq ="daily";
+	$path_kb = "http://fullhdfree.xyz/";
+	$slug = "video/";
+	$per_page = 10; // max size
 
-global $query;
-require '/custom.php';
-require '/split.php';
-$limit = 40000;
-// $path_kb = 'www.google.com';
-header( 'Cache-Control: public' );
-header( 'Expires: ' . gmdate( 'D, d M Y H:i:s', time() + 86400 ) . ' GMT' );
-header( "Content-type: text/xml" ); 
-	echo '<?xml version="1.0" encoding="UTF-8"?>';
-	echo '<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+	$fn = "1"; // file number, starts empty, then 1, 2, etc
+	
+
+		$prefix_string 	= "<url>\n<loc>";
+		$postfix_string	= "</loc>\n<priority>$priority</priority>\n<lastmod>$lastmodified</lastmod>\n<changefreq>$changefreq</changefreq>\n</url>\n";
+		$sitemap_header = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n".
+			"<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\" xmlns:xhtml=\"http://www.w3.org/1999/xhtml\" xmlns:image=\"http://www.google.com/schemas/sitemap-image/1.1\" xmlns:video=\"http://www.google.com/schemas/sitemap-video/1.1\">\n";
+		$sitemap_footer = "</urlset>\n";
+
+foreach ($files_name as $word)
+		{
+			$arraystrreplace= array("&"," ");
+			$strreplace = str_replace($arraystrreplace,"-",trim($word)); 
+			$sitemap_url_string []= "$prefix_string$path_kb$slug$strreplace"."$postfix_string";
+		}	
+		//delete all file in dir	
+		array_map('unlink',glob("./sitemap/*"));
+		
+		
+		$counter = 0;
+		while ($chunk = array_splice($sitemap_url_string,0,$per_page)){
+
+			$openfile=@fopen("./sitemap/sitemap".($counter++).$f,"wb");
+			$celeng = implode("",$chunk);
+			if($openfile){
+			@fwrite($openfile,$sitemap_header.$celeng.$sitemap_footer);
+			@fclose($openfile);
+			}
+		}
+	//	
+	
+	//make sitemap_index start here	
 	
 	$files = glob('sitemap/*.xml' );	
 	$totalfile = count($files);
 	echo $totalfile;
-		
-	
-	
 	foreach( $files as $file ) {
-		$line_count[] = lines_count( $file );
+		// $line_count[] = lines_count( $file );
+		$line_count[] = $totalfile;
 	}
 	$sitemapname = "sitemap";
 
-	$rows = array_sum( $line_count );
-	$total = ceil( $rows / $limit );
+	// $rows = array_sum($totalfile);
+	// $total = ceil( $rows / $limit );
 	$number = 0;
 foreach( $line_count as $key => $line ) {
 		// for( $i = 1; $i <= ( ceil( $line / $limit ) ); $i++ ) {
 		for ($i=1; $i< $totalfile; $i++ ){	
 			$number++;
-			// echo '
-			// <sitemap>
-				// <loc>' . $domain . '/sitemap/' . ( $key + 1 ) . '-' . $i . '.xml</loc>
-				// <lastmod>' . date( 'c' ) . '</lastmod>
-			// </sitemap>
-			// ';
 			$sitemap[]= '
 			<sitemap>
 				<loc>' . $path_kb . 'sitemap/' .$sitemapname. $i . '.xml</loc>
@@ -55,5 +79,4 @@ foreach( $line_count as $key => $line ) {
 	fwrite ($myfile,$space);
 	fwrite ($myfile,'</sitemapindex>');
 	echo '</sitemapindex>';
-	
 ?>
